15f41acd1f9cc672b880f6a233ee6801062da485
Alan Santos <alanssantos32@gmail.com>
2024-04-27 09:16:09 -0300

    Extração da classe feedback

---
+++ src/Model/Feedback.php
@@ -0,0 +1,24 @@
+<?
+
+class Feedback
+{
+    /**
+     * @var int
+     */
+    private $nota;
+
+    /**
+     * @var string
+     */
+    private $depoimento;
+
+    public function __construct(int $nota, ?string $depoimento)
+    {
+        if ($nota < 9 && empty($depoimento)) {
+            throw new \DomainException('Depoimento obrigatório');
+        }
+        
+        $this->nota = $nota;
+        $this->depoimento = $depoimento;
+    }
+}
